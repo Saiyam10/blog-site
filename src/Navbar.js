3fa2f2449805e8b2c6f87f2cbf73c5002bd5adca
Saiyam10 <saxenasaiyam@gmail.com>
2024-02-27 15:20:00 +0530

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+//Link tag instead of anchor tags are used in react webiste 
+// intercept the request to server and instead do it by itself 
+
+import {Link} from 'react-router-dom'
+import logo from './logo.png';
+
+const Navbar = () =>{
+    return (
+        <nav  className="navbar">
+         
+            <h1>My Blog</h1>
+            
+            
+              
+            <div className="links">
+                <Link to="/"> Home</Link>   
+                <Link to="/create"> New Blog</Link>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
